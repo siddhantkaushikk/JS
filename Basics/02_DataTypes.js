af347ea856fe97e137624b6872595fc607d8c5d5
siddhantkaushikk <121578443+siddhantkaushikk@users.noreply.github.com>
2024-03-11 18:59:36 +0000

    Datatypes

---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // when used, treats all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser so this way of using alert won't work
+
+console.log(3 
+    +
+     3) // code readability should be high but this is spoiling it
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//implicit data types
+// number => 2 to power 53
+// bigint=> for bigger numbers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => which is not yet been given value to
+// symbol => unique
+
+//non-implicit types
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
